f11c4ab1de04cf5b3e88bb0eb8c7cc97b7cf2d53
André Marques <andre.m.d.abreu@gmail.com>
2024-01-17 09:56:59 -0300

    Favicon e metatag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.png"
+          type="image/png"
+          sizes="256x256"
+        />
+        <meta name="author" content="André M. Abreu" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
