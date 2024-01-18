e0423c6e8fd27cd6dc7cb9f04e5352d624325bf7
André Marques <andre.m.d.abreu@gmail.com>
2024-01-18 11:19:45 -0300

    Resolução do desafio

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styled from "styled-components";
 
 export default function Menu() {
+  const nomeRota = usePathname();
   return (
     <StyledNav>
-      <Link href="/">Blog</Link>
-      <Link href="/produtos">Produtos</Link>
-      <Link href="/sobre">Sobre</Link>
-      <Link href="/contato">Contato</Link>
+      <Link className={nomeRota === "/" ? "ativo" : ""} href="/">
+        Blog
+      </Link>
+      <Link
+        className={nomeRota === "/produtos" ? "ativo" : ""}
+        href="/produtos"
+      >
+        Produtos
+      </Link>
+      <Link className={nomeRota === "/sobre" ? "ativo" : ""} href="/sobre">
+        Sobre
+      </Link>
+      <Link className={nomeRota === "/contato" ? "ativo" : ""} href="/contato">
+        Contato
+      </Link>
     </StyledNav>
   );
 }
@@ -16,6 +29,10 @@ const StyledNav = styled.nav`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  .ativo {
+    background-color: #000;
+  }
 
   a {
     text-decoration: none;
@@ -31,6 +48,16 @@ const StyledNav = styled.nav`
     &:last-child {
       border-top-right-radius: var(--borda-arredondada);
       border-bottom-right-radius: var(--borda-arredondada);
+    }
+
+    &:hover,
+    &:focus {
+      background-color: var(--cor-primaria-fundo-hover);
+    }
+
+    @media screen and (min-width: 700px) {
+      padding-left: 2rem;
+      padding-right: 2rem;
     }
   }
 `;
