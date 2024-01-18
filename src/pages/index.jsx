7770a6dd6f596788aca736ea75e1fe58522ed586
André Marques <andre.m.d.abreu@gmail.com>
2024-01-18 08:25:54 -0300

    Adição de metatags personalizadas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,19 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
-      <h1>PetShop</h1>
+      <Head>
+        <title>PetShop</title>
+        <meta
+          name="description"
+          content="Web App PetShop criado com Next.js como exemplo do curso Téc. Informática para Internet"
+        />
+        <meta name="keywords" content="PetShop, Banho, Ração, Gato, Cachorro" />
+      </Head>
+      <section>
+        <h2>Pet Notícias</h2>
+      </section>
     </>
   );
 }
