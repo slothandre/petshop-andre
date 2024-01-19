afa95f74566d765d8a54b20a5e52295f90f910ca
André Marques <andre.m.d.abreu@gmail.com>
2024-01-19 08:32:26 -0300

    Adição de conteúdo na página "sobre"

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -14,6 +14,25 @@ export default function Sobre() {
       </Head>
       <StyledSobre>
         <h2>Sobre nosso PetShop</h2>
+        <h3>Missão</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+          nam quas expedita, vero molestiae aliquid vitae molestias officiis
+          unde nisi dolore praesentium sunt rem? Facilis quas reprehenderit
+          reiciendis officia id.
+        </p>
+        <h3>Visão</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+          repudiandae ipsa maxime porro ab quasi expedita explicabo velit et
+          voluptates quod temporibus quibusdam quo culpa neque, corporis cumque
+          voluptatum vitae!
+        </p>
+        <h3>Valores</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente
+          ipsa maxime consequatur adipisci totam enim?
+        </p>
       </StyledSobre>
     </>
   );
