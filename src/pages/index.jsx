843cac07af1273479bfe36685cf4a9e098ce42eb
André Marques <andre.m.d.abreu@gmail.com>
2024-01-19 10:33:45 -0300

    Formatação do article e links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styled from "styled-components";
 
 export default function Home() {
@@ -14,10 +15,44 @@ export default function Home() {
       </Head>
       <StyledHome>
         <h2>Pet Notícias</h2>
+        <StyledListaPosts>
+          <article>
+            <Link href="">
+              <h3>Título do post...</h3>
+              <p>Subtítulo do post...</p>
+            </Link>
+          </article>
+        </StyledListaPosts>
       </StyledHome>
     </>
   );
 }
+
+const StyledListaPosts = styled.div`
+  article {
+    background-color: #f7f7f7;
+    padding: 1rem;
+    margin-bottom: 1rem;
+    box-shadow: var(--sombra-box);
+    border-radius: var(--borda-arredondada);
+    transition: transform 200ms;
+
+    & a {
+      text-decoration: none;
+      color: #000;
+
+      &:hover,
+      &:focus {
+        color: #06f;
+      }
+    }
+  }
+
+  article:hover {
+    cursor: pointer;
+    transform: scale(1.05);
+  }
+`;
 
 const StyledHome = styled.section`
   h2::before {
