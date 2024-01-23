e57dfed724bf5fa5bb7438dc8fa3e0c41a604aae
André Marques <andre.m.d.abreu@gmail.com>
2024-01-23 09:54:35 -0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,14 @@ import Head from "next/head";
 import styled from "styled-components";
 import ListaPosts from "@/components/ListaPosts";
 import { useState } from "react";
+import serverApi from "./api/server";
 
 /* Função getStaticProps
 Utilizada para execução de código server-side (neste cado, fetch na API)
 com o objetico de gerar props com os dados processados. */
 export async function getStaticProps() {
   try {
-    const resposta = await fetch(`http://10.20.46.20:2112/posts`);
+    const resposta = await fetch(`${serverApi}/posts`);
     const dados = await resposta.json();
 
     if (!resposta.ok) {
